cc47ae28bc2186266d0ea7978a18c10da52d74e1
pedro <pedro.jcaceresl@gmail.com>
2024-02-07 16:07:04 -0300

    Update ESLint and TS configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": [
+      "warn", { allowConstantExport: true }
     ],
-  },
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  }
 }
